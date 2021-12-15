727264f5064fa51625fa4867db71b0b8436452a6
Michael Ritsema <michaelritsema@gmail.com>
2021-12-15 15:22:16 -0600

    breaking macros into profiles and more utility funcs

--- Kee/Kee.lua
+++ Kee/Kee.lua
@@ -8,8 +8,26 @@ KeeSettings = {}
 KeeSettings.LEADER_NAME = "flogzero"
 
 
+
+local DeletePlayerMacros = function() 
+	for i=121,138 do DeleteMacro(i) end
+end
+
+local DeleteAccountMacros = function() 
+	--p,a = GetNumMacros();
+	--print(format("p macros: %i, a macros: %i",p,a));
+	for i=1,120 do DeleteMacro(i) end
+end
+
+local DeleteAllMacros = function() 
+	DeleteAccountMacros()
+	DeletePlayerMacros()
+end
+
+
+
 local CreateOrReplaceMacro = function(name,icon,macro_string,macro_type) 
-	--print(format("Loading macro  %s", name));
+	print(format("Loading macro  %s", name));
 	macro_string = gsub(macro_string, "{{LEADER_NAME}}", KeeSettings.LEADER_NAME)
 	DeleteMacro(name);
 	CreateMacro(name, icon, macro_string, macro_type);
@@ -21,10 +39,24 @@ local LoadMacro = function(k,v)
 end
 
 SlashCmdList.KEELOAD= function(msg, editBox)
+	if msg == "priest" then
+		print("Clearing player macros and loading priest.");
+		DeletePlayerMacros();
+		table.foreach(Kee.Priest.Macros, LoadMacro);
+	end
 
-	print("Loading macros");
-	table.foreach(Kee.KeeMacros, LoadMacro);
-	--local macroId = CreateOrReplaceMacro("MyMacro", "INV_MISC_QUESTIONMARK", "/cast Demon Armor;", nil);
+	if msg == "account" then
+		print("Clearing account macros and loading account");
+		DeleteAccountMacros();
+		table.foreach(Kee.Account.Macros, LoadMacro);
+	end
+
+	if msg == "shaman" then
+		print("Clearing player macros and loading shaman");
+		DeletePlayerMacros();
+		table.foreach(Kee.Shaman.Macros, LoadMacro);
+	end
+
 
 	print("Loaded Macros");
 end
--- Kee/KeeMacros.lua
+++ Kee/KeeMacros.lua
@@ -1,68 +1,133 @@
 local Kee = Kee
 
-Kee.KeeMacros = {}
+Kee.Account = {}
+Kee.Account.Macros = {}
+Kee.Priest = {}
+Kee.Priest.Macros = {}
+Kee.Shaman = {}
+Kee.Shaman.Macros = {}
 
 --CreateMacro(name, icon, macro_string, macro_type);
 
 -- variables used 
 -- LEADER_NAME: Multibox Leader, usually the human screen
 
+local addMacro = function (macro_table,name,icon,macro_string,macro_type) 
+    table.insert(macro_table, 
+    {
+        name=name,
+        icon=icon,
+        macro_string=macro_string,
+        macro_type=macro_type
+    }
+    );
+end
 
-Kee.KeeMacros[0] =  { 
-name = "buff",
-icon = "INV_MISC_QUESTIONMARK",
-macro_string = 
+addMacro(Kee.Priest.Macros, "buff", "INV_MISC_QUESTIONMARK", 
 [[
 #showtooltip
 /assist {{LEADER_NAME}}
 /castsequence  Power Word: Fortitude
 ]],
-macro_type=1
-};
+1
+);
 
-Kee.KeeMacros[1] =  { 
-    name = "cleanse",
-    icon = "INV_MISC_QUESTIONMARK",
-    macro_string = 
+addMacro(Kee.Priest.Macros, "cleanse", "INV_MISC_QUESTIONMARK", 
 [[
 #showtooltip
 /assist {{LEADER_NAME}}
 /cast Abolish Disease
- ]],
-    macro_type=1
-};
+]],
+1
+);
 
-
-Kee.KeeMacros[2] =  { 
-    name = "dispel",
-    icon = "INV_MISC_QUESTIONMARK",
-    macro_string = 
+addMacro(Kee.Priest.Macros, "dispel", "INV_MISC_QUESTIONMARK", 
 [[
 #showtooltip
 /assist {{LEADER_NAME}}
 /cast Dispel Magic
 ]],
-    macro_type=1
-};
+1
+);
 
 
-Kee.KeeMacros[3] =  { 
-    name = "heal alt",
-    icon = "INV_MISC_QUESTIONMARK",
-    macro_string = 
+
+addMacro(Kee.Priest.Macros, "heal alt", "INV_MISC_QUESTIONMARK", 
 [[
 #showtooltip
 /castsequence [target=player] reset=15 Power Word: Shield, Heal
 ]],
-    macro_type=1
-};
+1
+);
 
-Kee.KeeMacros[4] =  { 
-    name = "heal leader",
-    icon = "INV_MISC_QUESTIONMARK",
-    macro_string = 
+
+addMacro(Kee.Priest.Macros, "heal leader", "INV_MISC_QUESTIONMARK", 
 [[
 /cast [target={{LEADER_NAME}}]Heal
 ]],
-    macro_type=1
-};
+1
+);
+
+addMacro(Kee.Shaman.Macros,
+    "heal leader",
+    "INV_MISC_QUESTIONMARK",
+[[
+/cast [target={{LEADER_NAME}}]Healing Wave
+]],
+    1
+
+);
+
+addMacro(Kee.Account.Macros, "follow", "INV_MISC_QUESTIONMARK",
+[[
+/follow {{LEADER_NAME}}
+]],
+nil
+);
+
+addMacro(Kee.Account.Macros,
+ "reload ui",
+   "INV_MISC_QUESTIONMARK",
+
+[[
+/reload ui
+]],
+nil
+);
+
+addMacro(Kee.Account.Macros,
+
+  "petattack",
+    "INV_MISC_QUESTIONMARK",
+ 
+[[
+#showtooltip
+/assist {{LEADER_NAME}}
+/petpassive
+/petattack
+]],
+  nil
+);
+
+addMacro(Kee.Account.Macros,
+
+ "petfollow",
+  "INV_MISC_QUESTIONMARK",
+
+[[
+#showtooltip
+/petfollow
+/petpassive
+]],
+  nil
+);
+
+addMacro(Kee.Account.Macros,
+"mnt",
+"INV_MISC_QUESTIONMARK",
+[[
+#showtooltip
+/use Swift White Steed
+]],
+nil
+);
